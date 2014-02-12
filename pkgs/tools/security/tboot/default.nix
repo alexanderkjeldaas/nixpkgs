b1faea4d8683e86592ffd47760ecef4d5816536b
Alexander Kjeldaas <ak@formalprivacy.com>
2014-02-12 01:43:23 +0100

    Updated to tboot-1.8.0. Added lcp_writepol -Z option.

--- pkgs/tools/security/tboot/default.nix
+++ pkgs/tools/security/tboot/default.nix
@@ -1,14 +1,16 @@
 {stdenv, fetchurl, autoconf, automake, trousers, openssl, zlib}:
 
 stdenv.mkDerivation {
-  name = "tboot-1.7.4";
+  name = "tboot-1.8.0";
 
   src = fetchurl {
-    url = https://sourceforge.net/projects/tboot/files/tboot/tboot-1.7.4.tar.gz;
-    sha256 = "0ix970w27nzgh2rcz0xwxwdp5rlw1n2705nyd9nlj44rspfni327";
+    url = https://sourceforge.net/projects/tboot/files/tboot/tboot-1.8.0.tar.gz;
+    sha256 = "04z1maryqnr714f3rcynqrpmlx76lxr6bb543xwj5rdl1yvdw2xr";
   };
 
   buildInputs = [ trousers openssl zlib ];
+
+  patches = [ ./tboot-add-well-known-secret-option-to-lcp_writepol.patch ];
 
   configurePhase = ''
     for a in lcptools utils tb_polgen; do
