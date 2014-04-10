16cc0bdcbf7ae587b7871e4317db0d4386b9a9b7
Alexander Kjeldaas <ak@formalprivacy.com>
2014-04-10 13:46:15 +0200

    Remove dates from kernel 3.10.35

--- pkgs/os-specific/linux/kernel/linux-3.10.nix
+++ pkgs/os-specific/linux/kernel/linux-3.10.nix
@@ -1,4 +1,11 @@
-{ stdenv, fetchurl, ... } @ args:
+{ stdenv, fetchurl, kernelPatches ? [], ... } @ args:
+
+let
+  patches = kernelPatches ++
+   [{ name = "remove-driver-compilation-dates";
+      patch = ./linux-3-10-35-no-dates.patch;
+    }];
+in
 
 import ./generic.nix (args // rec {
   version = "3.10.36";
@@ -8,6 +15,8 @@ import ./generic.nix (args // rec {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${version}.tar.xz";
     sha256 = "1bx94v829qvv5r9h6cj5ddyj5n6qddy8bppnl8minjnqsv5l0vnr";
   };
+
+  kernelPatches = patches;
 
   features.iwlwifi = true;
   features.efiBootStub = true;
