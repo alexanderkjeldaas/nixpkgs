f0c1e5e669fa7be3be97faf9414ff0f8f5269021
Alexander Kjeldaas <ak@formalprivacy.com>
2014-04-11 10:07:07 +0200

    Add useFakeTime for python, groff, kernel.

--- pkgs/development/interpreters/python/2.7/default.nix
+++ pkgs/development/interpreters/python/2.7/default.nix
@@ -72,7 +72,7 @@ let
 
     NIX_CFLAGS_COMPILE = optionalString stdenv.isDarwin "-msse2";
     DETERMINISTIC_BUILD = 1;
-
+    useFakeTime = 1;
     setupHook = ./setup-hook.sh;
 
     postInstall =
