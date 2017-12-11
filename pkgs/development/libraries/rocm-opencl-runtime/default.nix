b925fcc155a7d1e41a0c0cc6cb1ea154b9db50a5
Alexander Kjeldaas <ak@formalprivacy.com>
2017-12-11 19:25:30 +0100

    rocm-opencl-runtime update.

--- pkgs/development/libraries/rocm-opencl-runtime/default.nix
+++ pkgs/development/libraries/rocm-opencl-runtime/default.nix
@@ -1,5 +1,8 @@
 { stdenv, fetchgit, libelf, roct, cmake, pkgconfig, python
-, fetchRepoProject
+, fetchRepoProject, git
+, rocr-runtime
+, cacert
+, mesa
 }:
 
 stdenv.mkDerivation rec {
@@ -10,29 +13,32 @@ stdenv.mkDerivation rec {
   src = fetchRepoProject {
     name = "ROCm-OpenCL-Runtime";
 #    url    = "https://github.com/RadeonOpenCompute/ROCm-OpenCL-Runtime.git";
-    rev    = "5c209b3df0d7ebeedcf803471ab22f36b49b1cee";
+#    rev    = "roc-1.7.x"; #5c209b3df0d7ebeedcf803471ab22f36b49b1cee";
+    rev    = "master"; #5c209b3df0d7ebeedcf803471ab22f36b49b1cee";
     manifest    = "https://github.com/RadeonOpenCompute/ROCm-OpenCL-Runtime.git";
     manifestName = "opencl.xml";
 #    manifest = "opencl.xml";
 #    manifest = "https://raw.githubusercontent.com/RadeonOpenCompute/ROCm-OpenCL-Runtime/roc-1.6.x/opencl.xml/"
-    sha256 = "1y1y3asx92nnzz9pcp46gwf3a3iq9xs80x812aaraxv9mg1nil4p";
+    sha256 = "1j0cqsqm9lyi4lyljddridp8x6iarhcj3zydfdlzlf9qvinvbda9";
   };
 
 #  phases = [ "unpackPhase" "installPhase"];
 
 
   buildInputs =
-    [ cmake pkgconfig roct libelf python
+    [ cmake pkgconfig roct libelf python git mesa
     ];
 
-#  prePatch = "cd src;";
+  prePatch = "cd opencl;";
+  GIT_SSL_CAINFO="/etc/ssl/certs/ca-certificates.crt";
+  SSL_CERT_FILE="/etc/ssl/certs/ca-certificates.crt";
 
 
   enableParallelBuilding = true;
   # the cmake package does not handle absolute CMAKE_INSTALL_INCLUDEDIR correctly
   # (setting it to an absolute path causes include files to go to $out/$out/include,
   #  because the absolute path is interpreted with root at $out).
-  cmakeFlags = "-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-I${roct}/include:${libelf}/include";
+  cmakeFlags = "-DCMAKE_BUILD_TYPE=Release -DROCR_INCLUDE_DIR=${rocr-runtime}/src/inc -DROCR_LIBRARY=${rocr-runtime}/lib"; # -DCMAKE_CXX_FLAGS=-I${roct}/include:${libelf}/include";
 
 
   meta = {
